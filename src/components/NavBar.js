87a3f9dedb7531288c7a0194c3620532586ded85
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-19 14:26:54 -0400

    still working on navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,48 @@ import { NavLink } from 'react-router-dom'
 
 function NavBar() {
     return (
-       <header>
-           <div>
-               <nav>
-                    <NavLink to='/' exact>
+       <header className="bg-green-600">
+           <div className="container mx-auto flex justify-between">
+               <nav className="flex">
+                    <NavLink 
+                        to='/' 
+                        exact 
+                        activeClassName="text-white-100"
+                        // activeClassName is only used when user is on the linked page
+                        className="inline-flex items-center py-6 px-3 mr-4 text-blue-100 hover:text-green-800 text-4xl font-bold cursive tracking-wide"
+                    >
                         Seth
                     </NavLink>
-                    <NavLink to='/post'>
+
+                    <NavLink 
+                        to='/post' 
+                        activeClassName="text-white-100 bg-green-700"
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800'
+                    >
                         Blog Posts
                     </NavLink >
-                    <NavLink to='/project'>
+                    
+                    <NavLink 
+                        to='/project' 
+                        activeClassName="text-white-100 bg-green-700"
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800'
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to='/about'>
+
+                    <NavLink 
+                        to='/about' 
+                        activeClassName="text-white-100 bg-green-700"
+                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover:text-green-800'
+                    >
                         About Me!
                     </NavLink>
                </nav>
+               
+               {/* going to use package react-social-icons */}
+               <div>
+
+               </div>
            </div>
        </header> 
     )
