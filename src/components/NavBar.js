3c9e4e8b6f8c9f061fdee96f15683ef457c41db9
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-19 14:05:21 -0400

    skeleton of navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
-    return <h1>NavBar Page</h1>
+    return (
+       <header>
+           <div>
+               <nav>
+                    <NavLink to='/' exact>
+                        Seth
+                    </NavLink>
+                    <NavLink to='/post'>
+                        Blog Posts
+                    </NavLink >
+                    <NavLink to='/project'>
+                        Projects
+                    </NavLink>
+                    <NavLink to='/about'>
+                        About Me!
+                    </NavLink>
+               </nav>
+           </div>
+       </header> 
+    )
+
 }
 
 
