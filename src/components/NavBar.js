bc2d2b0bc8e3787276911413b9296f7f1f7b462b
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-20 07:58:41 -0400

    added social icon package

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 function NavBar() {
     return (
@@ -43,7 +44,9 @@ function NavBar() {
                
                {/* going to use package react-social-icons */}
                <div>
-
+                    <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }}/>
+                    <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }}/>
+                    <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35 }}/>
                </div>
            </div>
        </header> 
