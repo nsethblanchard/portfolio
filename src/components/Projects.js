1128b97518ef656b1ae0248b7e643c164b26a853
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-23 13:57:59 -0400

    mostly done with styling for projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,6 +24,9 @@ function Projects() {
                 <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Projects Page!</h2>
                 <section className='grid grid-cols-2 gap-8'>
+                    
+                    {projectData && projectData.map((project, index) => (
+                    
                     <article className='relative rounded-lg shadow-xl bg-white p-16'>
                         <h3 className='text-gray-800 tex-3xl font-bold mb-2 hover:text-red-700'> 
                             <a 
@@ -50,10 +53,18 @@ function Projects() {
                                 <strong classname='font-bold'>Type</strong>:{' '}
                                 {project.projectType}
                             </span>
-                            <p></p>
-                            <span></span>
+                            <p className="my-6 text-lg text-gray-700 leading-relaxed">{project.description}</p>
+                            <a href={project.link} 
+                                rel="noopener noreferrer" 
+                                target="_blank" 
+                                className="text-red-500 font-bold hover:underline hover:text-red-400"
+                            >
+                                View The Project{' '}
+                                <span role="img" aria-label="right pointer">👉</span>
+                            </a>
                         </div>
                     </article>
+                    ))}
                 </section>
             </section>
         </main>
