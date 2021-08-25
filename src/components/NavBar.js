66a6656cf74ba5ca7b82e15d1a000bb6564572b2
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-25 09:50:33 -0400

    styling changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,15 +19,15 @@ function NavBar() {
 
                     <NavLink 
                         to='/posts' 
-                        activeClassName="text-white-100 bg-green-700"
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                        activeClassName="text-white-100 bg-gray-700"
+                        className='inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
                     >
                         Blog Posts
                     </NavLink >
                     
                     <NavLink 
                         to='/projects' 
-                        activeClassName="text-white-100 bg-green-700"
+                        activeClassName="text-white-100 bg-gray-700"
                         className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
                     >
                         Projects
@@ -35,7 +35,7 @@ function NavBar() {
 
                     <NavLink 
                         to='/about' 
-                        activeClassName="text-white-100 bg-green-700"
+                        activeClassName="text-white-100 bg-gray-700"
                         className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
                     >
                         About
