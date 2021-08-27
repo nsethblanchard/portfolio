562cfe494ff704a5de9960af6f957adc7821fec8
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-27 11:57:21 -0400

    more styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { SocialIcon } from 'react-social-icons';
 
 function NavBar() {
     return (
-       <header className="bg-gray-600 h-20">
+       <header className="bg-gray-600 h-24">
            <div className="container mx-auto flex justify-between">
                <nav className="flex">
                     <NavLink 
@@ -12,7 +12,7 @@ function NavBar() {
                         exact 
                         activeClassName="text-white-100"
                         // activeClassName is only used when user is on the linked page
-                        className="items-center py-6 px-3 mr-4 text-blue-100 hover-nav text-3xl font-bold cursive tracking-wide"
+                        className="items-center py-6 px-3 mr-4 mt-2 text-blue-100 hover-nav text-3xl font-bold cursive tracking-wide"
                     >
                         Seth Blanchard
                     </NavLink>
@@ -20,7 +20,7 @@ function NavBar() {
                     <NavLink 
                         to='/projects' 
                         activeClassName="text-white-100 bg-gray-700"
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                        className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
                     >
                          Projects
                     </NavLink>
@@ -28,7 +28,7 @@ function NavBar() {
                     <NavLink 
                         to='/activities' 
                         activeClassName="text-white-100 bg-gray-700"
-                        className='inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                        className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
                     >
                         Extra-curricular
                     </NavLink >
@@ -36,7 +36,7 @@ function NavBar() {
                     <NavLink 
                         to='/resume' 
                         activeClassName="text-white-100 bg-gray-700"
-                        className='inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                        className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
                     >
                         Resume
                     </NavLink >
@@ -44,17 +44,17 @@ function NavBar() {
                     <NavLink 
                         to='/about' 
                         activeClassName="text-white-100 bg-gray-700"
-                        className='inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                        className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
                     >
                         Bio
                     </NavLink>
                </nav>
                
                {/* going to use package react-social-icons */}
-               <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon url="https://github.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 25, width: 25 }}/>
-                    <SocialIcon url="https://www.linkedin.com/in/seth-blanchard-2042ba89/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 25, width: 25 }}/>
-                    <SocialIcon url="https://twitter.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 25, width: 25 }}/>
+               <div className="inline-flex py-3 px-4 my-6">
+                    <SocialIcon url="https://github.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
+                    <SocialIcon url="https://www.linkedin.com/in/seth-blanchard-2042ba89/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
+                    <SocialIcon url="https://twitter.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
                </div>
            </div>
        </header> 
