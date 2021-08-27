4a25ea2bdacd09a2e58e4be9c01f42fc15cbce9c
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-27 14:51:24 -0400

    hard-coded about bio because couldnt get fetch to grab all text

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ function Resume() {
     return (
         <main className='bg-gray-400'>
             <div className='bg-gray-400 pt-16 pb-24 mr-60 ml-60'>
-                <img classname="object-fill" src={resume} alt="Seth Blanchard's Resume"/>
+                <img className="object-fill" src={resume} alt="Seth Blanchard's Resume"/>
             </div>
         </main>
     )
