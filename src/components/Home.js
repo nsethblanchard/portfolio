d7463b96df4ce7aa422dc1d6e8d4323535d60826
Seth Blanchard <nsethblanchard@gmail.com>
2021-09-07 10:09:53 -0400

    small pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
     return (
        <main>
            <img src={image} alt="Hammocks and Mountain Views" className="absolute object-cover w-full h-full"/>
-           <img src={profileImage} alt="My ugly mug" className="relative transform md:scale-50 face rounded-full opacity-90"/>
+           <img src={profileImage} alt="My ugly mug" className="relative transform sm:pt-20 scale-50 face rounded-full opacity-90"/>
            
            <section className="relative px-48 -mt-36 pb-12 justify-center grid grid-cols-8">
                 <img
