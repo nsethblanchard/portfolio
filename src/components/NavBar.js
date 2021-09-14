accca3f04c244cc264e35d7744747086ca5a82b9
Seth Blanchard <nsethblanchard@gmail.com>
2021-09-14 08:09:07 -0400

    all except active hamburger!

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,22 +4,56 @@ import { SocialIcon } from "react-social-icons";
 
 function NavBar() {
   return (
-    <div className="bg-gray-600 antialiased">
-      <header className="flex justify-between bg-gray-700 px-4 py-3 text-white">
+    <header className="bg-gray-600 md:h-24">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
         <NavLink
           to="/"
           exact
           activeClassName="text-white-100"
           // activeClassName is only used when user is on the linked page
-          className="items-center py-6 px-3 mr-4 mt-2 text-blue-100 text-3xl font-bold cursive tracking-wide"
+          className="items-center py-6 px-3 mr-4 mt-2 text-blue-100 hover-nav text-3xl font-bold cursive tracking-wide"
         >
           Seth Blanchard
         </NavLink>
-        <div className="flex">
+
+        <NavLink
+            to='/projects'
+            activeClassName="text-white-100 bg-gray-700"
+            className='hidden md:flex text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+        >
+            Projects
+        </NavLink>
+
+        <NavLink
+                      to='/resume'
+                      activeClassName="text-white-100 bg-gray-700"
+                      className='hidden md:flex text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                  >
+                      Resume
+                  </NavLink >
+
+                  <NavLink
+                      to='/about'
+                      activeClassName="text-white-100 bg-gray-700"
+                      className='hidden md:flex text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                  >
+                      About
+                  </NavLink>
+
+                  <NavLink
+                      to='/activities'
+                      activeClassName="text-white-100 bg-gray-700"
+                      className='hidden md:flex text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                  >
+                      Activities
+                  </NavLink >
+        
+        <div className="flex pt-2 md:hidden">
           {/* hamburger icon */}
           <button>
             <svg
-              className="mt-3 h-8 w-8 fill-current text-gray-500 hover:text-gray-200 md:mr-10"
+              className="h-8 w-8 mx-auto fill-current text-gray-500 hover:text-gray-200"
               fill="#000000"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 30 30"
@@ -30,8 +64,17 @@ function NavBar() {
             </svg>
           </button>
         </div>
-      </header>
-    </div>
+
+        </nav>
+        <div className="inline-flex pt-7 md:py-3 px-4 my-6">
+          <SocialIcon url="https://github.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
+          <SocialIcon url="https://www.linkedin.com/in/seth-blanchard-2042ba89/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
+          <SocialIcon url="https://twitter.com/nsethblanchard" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 32, width: 32 }}/>
+        </div>
+
+
+      </div>
+    </header>
   );
 
   //    <header className="bg-gray-600 w-full">
@@ -47,37 +90,37 @@ function NavBar() {
   //                     Seth Blanchard
   //                 </NavLink>
 
-  //                 <NavLink
-  //                     to='/projects'
-  //                     activeClassName="text-white-100 bg-gray-700"
-  //                     className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
-  //                 >
-  //                      Projects
-  //                 </NavLink>
+                  // <NavLink
+                  //     to='/projects'
+                  //     activeClassName="text-white-100 bg-gray-700"
+                  //     className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                  // >
+                  //      Projects
+                  // </NavLink>
 
-  //                 <NavLink
-  //                     to='/resume'
-  //                     activeClassName="text-white-100 bg-gray-700"
-  //                     className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
-  //                 >
-  //                     Resume
-  //                 </NavLink >
+                  // <NavLink
+                  //     to='/resume'
+                  //     activeClassName="text-white-100 bg-gray-700"
+                  //     className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                  // >
+                  //     Resume
+                  // </NavLink >
 
-  //                 <NavLink
-  //                     to='/about'
-  //                     activeClassName="text-white-100 bg-gray-700"
-  //                     className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
-  //                 >
-  //                     About
-  //                 </NavLink>
+                  // <NavLink
+                  //     to='/about'
+                  //     activeClassName="text-white-100 bg-gray-700"
+                  //     className='text-2xl inline-flex items-center py-3 px-3 my-6 rounded text-blue-200 hover-nav'
+                  // >
+                  //     About
+                  // </NavLink>
 
-  //                 <NavLink
-  //                     to='/activities'
-  //                     activeClassName="text-white-100 bg-gray-700"
-  //                     className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
-  //                 >
-  //                     Activities
-  //                 </NavLink >
+                  // <NavLink
+                  //     to='/activities'
+                  //     activeClassName="text-white-100 bg-gray-700"
+                  //     className='text-2xl inline-flex items-center py-2 px-2 my-6 rounded text-blue-200 hover-nav'
+                  // >
+                  //     Activities
+                  // </NavLink >
 
   //            </nav>
 
